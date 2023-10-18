5f96494f47aa19100128f75e5c9a8d8e0da581f6
Sergio Parra <109539011+sergio2732@users.noreply.github.com>
2023-10-18 11:43:07 -0500

    Add files via upload

---
+++ src/main/java/co/edu/hospedaje/entidades/Transaccion.java
@@ -0,0 +1,28 @@
+package co.edu.hospedaje.entidades;
+
+public class Transaccion {
+    private String badgeOrin;
+    private String badgeDest;
+    private long amountOrin;
+    private long tasa;
+
+    public Transaccion(String badgeOrin, String badgeDest, long amountOrin, long tasa) {
+        this.badgeOrin = badgeOrin;
+        this.badgeDest = badgeDest;
+        this.amountOrin = amountOrin;
+        this.tasa = tasa;
+    }
+    public long changeDange(){
+        return (this.amountOrin*tasa);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaccion{" +
+                "badgeOrin='" + badgeOrin + '\'' +
+                ", badgeDest='" + badgeDest + '\'' +
+                ", amountOrin=" + amountOrin +
+                ", tasa=" + tasa +
+                '}';
+    }
+}
---
+++ src/main/java/co/edu/hospedaje/transaccion.java
@@ -0,0 +1,14 @@
+package co.edu.hospedaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class transaccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaccion);
+    }
+}
