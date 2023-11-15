35b218e8aba1317e65de072f4f0438435a7064f8
Sergio Parra <109539011+sergio2732@users.noreply.github.com>
2023-11-14 20:24:21 -0500

    Update MainActivity.java

--- main/java/co/edu/permiso1/MainActivity.java
+++ main/java/co/edu/permiso1/MainActivity.java
@@ -1,8 +1,10 @@
 package co.edu.permiso1;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -14,25 +16,24 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_CODE = 69;
-    //1. Declaracion de objetos del View a utilizar
+    public static final int REQUEST_CODE = 100;
     private Button btnCheckPermissions;
     private Button btnRequestPermissions;
     private TextView tvCamera;
     private TextView tvBT;
-    private TextView tvBiometric;
-    private TextView tvExeternalWrite;
-    private TextView tvReadExternal;
+    private TextView tvDactilar;
+    private TextView tvEws;
+    private TextView tvRS;
     private TextView tvInternet;
     private TextView tvResponse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //3. Llamado de enlace de datos
         begin();
-        //4. Enlace de los botones a metodos
         btnCheckPermissions.setOnClickListener(this::voidCheckPermissions);
         btnRequestPermissions.setOnClickListener(this::voidRequestPermissions);
     }
@@ -45,21 +46,19 @@ public class MainActivity extends AppCompatActivity {
         int statusRES = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
         int statusInternet = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET);
         int statusBiometric = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.USE_BIOMETRIC);
-        //Tarea: verificar si el status  se concede o no.(con un conficional if y set.text)
         tvCamera.setText("status camera: "+statusCamera);
         tvBT.setText("status BT: "+statusBt);
         tvInternet.setText("status Internet: "+statusInternet);
-        tvBiometric.setText("status Biometic: "+statusBiometric);
-        tvReadExternal.setText("status RE: "+statusRES);
-        tvExeternalWrite.setText("status EWS: "+statusWES );
+        tvDactilar.setText("status Biometic: "+statusBiometric);
+        tvRS.setText("status RE: "+statusRES);
+        tvEws.setText("status EWS: "+statusWES );
     }
-    //6. Solicitud de permiso de usuario
     private void voidRequestPermissions(View view) {
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},REQUEST_CODE);
         }
     }
-    //7. Gestio de respuesta del usuario
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -88,17 +87,17 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    //2. Enlace de objetos
+
     private void begin(){
         this.btnCheckPermissions = findViewById(R.id.btnCheckPermission);
-        this.btnRequestPermissions =findViewById(R.id.btnRequestPermission);
-        this.tvBiometric = findViewById(R.id.tvDactilar);
+        this.btnRequestPermissions = findViewById(R.id.btnRequestPermission);
+        this.tvDactilar = findViewById(R.id.tvFingerprint);
         this.tvBT = findViewById(R.id.tvBT);
         this.tvCamera = findViewById(R.id.tvCamera);
         this.tvInternet = findViewById(R.id.tvInternet);
-        this.tvExeternalWrite = findViewById(R.id.tvEws);
+        this.tvEws = findViewById(R.id.tvEws);
         this.tvResponse = findViewById(R.id.tvResponse);
-        this.tvReadExternal = findViewById(R.id.tvRS);
+        this.tvRS = findViewById(R.id.tvReadStorage);
         btnRequestPermissions.setEnabled(true);
     }
 }
